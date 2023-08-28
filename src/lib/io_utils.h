cb8df573727bea3bf260ffb657c26cff50b9dabf
Christoph Weidinger <christoph.weidinger@ait.ac.at>
2023-08-28 08:58:31 +0000

    added function descriptions

--- src/lib/io_utils.h
+++ src/lib/io_utils.h
@@ -22,20 +22,21 @@ Eigen::MatrixXd ExtractMatrix(const pdal::PointViewPtr view, const bool& with_no
 // Return string with fields in pointcloud
 std::string PointcloudFieldsToString(const pdal::PointViewPtr view);
 
+// Save pointcloud to file
 void SaveMatrixToFile(const Eigen::MatrixXd& A,
                       const std::string& path_in,
                       const std::string& path_out);
 
+// Create PDAL reader signature from a file extension
 std::string CreatePDALReaderType(const std::string& extension);
 
+// Create PDAL writer signature from a file extension
 std::string CreatePDALWriterType(const std::string& extension);
 
+// Create PDAL writer options from a file extension
 pdal::Options CreatePDALWriterOptions(const std::string& extension);
 
+// Overwrite the transformed x,y,z coordinates to the original pointcloud
 void UpdateTransformedPointcloud(const pdal::PointViewPtr view, const Eigen::MatrixXd& x_updated);
-
-void CheckNumColsOfMatrix(const Eigen::MatrixXd& M,
-                          const int& num_cols,
-                          const std::string& matrix_name);
 
 #endif  // IO_UTILS_H
